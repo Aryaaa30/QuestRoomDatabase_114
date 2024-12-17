125d39829cb8420dc586c295dd5c350de72a5f9e
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-17 10:55:08 +0700

    Implementasikan pemetaan data entity `Mahasiswa` ke UI dalam fungsi `toDetailUiEvent()`.

--- app/src/main/java/com/example/session10_roomlocaldbpart1/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/session10_roomlocaldbpart1/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,12 +2,24 @@ package com.example.session10_roomlocaldbpart1.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.session10_roomlocaldbpart1.data.entity.Mahasiswa
 import com.example.session10_roomlocaldbpart1.repository.RepositoryMhs
+import com.example.session10_roomlocaldbpart1.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
 }
 
@@ -22,4 +34,20 @@ data class DetailUiState(
 
     val isUiEventNotEmpty: Boolean
         get() = detailUiEvent != MahasiswaEvent()
+}
+
+/*
+ * Data class untuk menampung data yang akan ditampilkan di UI
+ */
+
+// memindahkan data dari entity ke ui
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
