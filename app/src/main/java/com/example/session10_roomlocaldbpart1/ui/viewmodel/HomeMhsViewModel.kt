ab03fbe644c1243b4a56fcc58b73263e11c1e629
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-17 10:35:20 +0700

    Tambahkan `HomeMhsViewModel` untuk mengelola state data mahasiswa dari Repository.

---
+++ app/src/main/java/com/example/session10_roomlocaldbpart1/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.session10_roomlocaldbpart1.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.session10_roomlocaldbpart1.repository.RepositoryMhs
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+
+
+}
