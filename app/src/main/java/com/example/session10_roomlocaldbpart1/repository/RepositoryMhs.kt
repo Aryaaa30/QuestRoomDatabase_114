6fa7ba26d6d8cec40fe8c3b79b88b0da6694c4a0
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-17 10:20:46 +0700

    Integrasikan fungsi-fungsi dari MahasiswaDao ke dalam RepositoryMhs.

--- app/src/main/java/com/example/session10_roomlocaldbpart1/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/session10_roomlocaldbpart1/repository/RepositoryMhs.kt
@@ -1,8 +1,14 @@
 package com.example.session10_roomlocaldbpart1.repository
 
 import com.example.session10_roomlocaldbpart1.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
 
