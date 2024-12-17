9eca7e2d68e5b1184e540583eeabffcbe571d6cb
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-17 10:40:32 +0700

    Tambahkan data class `HomeUiState` sebagai representasi state UI di halaman Home.

--- app/src/main/java/com/example/session10_roomlocaldbpart1/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/session10_roomlocaldbpart1/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.session10_roomlocaldbpart1.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.session10_roomlocaldbpart1.data.entity.Mahasiswa
 import com.example.session10_roomlocaldbpart1.repository.RepositoryMhs
 
-class HomeMhsViewModel(
+class HomeMhViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel(){
 
-
 }
+
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
