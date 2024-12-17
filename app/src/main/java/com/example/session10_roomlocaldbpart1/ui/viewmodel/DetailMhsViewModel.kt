f5edaadad3ce5889381ab4765b379a3c9c4a1191
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-12-17 10:52:09 +0700

    Update `DetailUiState` dengan properti tambahan untuk memeriksa apakah event UI kosong atau tidak.

--- app/src/main/java/com/example/session10_roomlocaldbpart1/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/session10_roomlocaldbpart1/ui/viewmodel/DetailMhsViewModel.kt
@@ -10,3 +10,16 @@ class DetailMhsViewModel(
 ) : ViewModel() {
 
 }
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
